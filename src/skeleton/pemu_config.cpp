15298e9e32d21e7dbc04e2138a7811c9f462f964
liujiny <liujinying@hunnu.edu.cn>
2026-08-19 21:49:53 +0800

    Add cheat OSD menu, independent autofire, and null-safety fixes

--- src/skeleton/pemu_config.cpp
+++ src/skeleton/pemu_config.cpp
@@ -115,6 +115,13 @@ PEMUConfig::PEMUConfig(Renderer *renderer, const std::string &name, int version)
     } else {
         emu_grp.addOption({"EFFECT", {"c2d-texture"}, 0, EMU_SHADER})->setFlags(HIDDEN);
     }
+    
+    // [连发菜单项 / Autofire Menu Option] 在菜单中加入 AUTOFIRE 选项（默认 OFF，索引 0）
+    emu_grp.addOption({"AUTOFIRE_A", {"OFF", "ON"}, 1, EMU_AUTOFIRE_A});
+    emu_grp.addOption({"AUTOFIRE_B", {"OFF", "ON"}, 0, EMU_AUTOFIRE_B});
+    // [作弊开关选项 / Cheats Option] 默认设为 1 (ON)
+    emu_grp.addOption({"CHEATS", {"OFF", "ON"}, 1, EMU_CHEATS});
+
 #ifdef __VITA__
     emu_grp.addOption({"WAIT_RENDERING", {"OFF", "ON"}, 1, EMU_WAIT_RENDERING});
 #endif
